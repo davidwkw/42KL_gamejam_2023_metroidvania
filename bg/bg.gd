d81697f806c64d492b3f0d62b10713d5559eece6
Wang Khang Waei <devtesting404@gmail.com>
2023-11-26 00:06:06 +0800

    update: add 2 additional parallax layers to background

--- bg/bg.gd
+++ bg/bg.gd
@@ -1,6 +1,6 @@
 extends ParallaxBackground
 
-const SCROLL_SPEED = 100
+const SCROLL_SPEED = 300
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
