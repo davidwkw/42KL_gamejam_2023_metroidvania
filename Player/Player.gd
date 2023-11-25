8536e4dd10775550367133e1f9f3874cdff38532
Wang Khang Waei <devtesting404@gmail.com>
2023-11-25 17:37:26 +0800

    update: limit player camera, add additional sprite animations

--- Player/Player.gd
+++ Player/Player.gd
@@ -6,33 +6,48 @@ const JUMP_VELOCITY = -400.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-@onready var anim: AnimatedSprite2D = get_node("AnimatedSprite2D")
+
+@onready var anim_player: AnimationPlayer = get_node("AnimationPlayer")
 
 func _ready():
-	anim.play("Idle")
+	anim_player.play("Idle")
 
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
-		anim.play("Jump")
+		if velocity.y > 0:
+			anim_player.play("Fall")
 
 	# Handle Jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		anim_player.play("Jump")
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("ui_left", "ui_right")
+	
+	direction_handler(direction)
+
 	if direction:
 		velocity.x = direction * SPEED
-		if direction < 0:
-			anim.flip_h = true
-		else:
-			anim.flip_h = false
-		anim.play("Run")
+		if velocity.y == 0:
+			anim_player.play("Run")
+
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-		anim.play("Idle")
+		if velocity.y == 0:
+			anim_player.play("Idle")
 
 	move_and_slide()
+	
+func direction_handler(direction : int):
+	var anim_sprite : AnimatedSprite2D = get_node("AnimatedSprite2D")
+	
+	if direction > 0:
+		anim_sprite.flip_h = false
+	elif direction < 0:
+		anim_sprite.flip_h = true
+	else: 
+		pass
