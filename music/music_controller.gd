f98999c95b8defd5d376a187dc57c1c3611f750d
Wang Khang Waei <devtesting404@gmail.com>
2023-11-29 22:18:36 +0800

    Revert "update: decrease volume of bgm by 20db on play"
    This reverts commit 13c55cb904a4b6cd1ef2d36cdd390335117a5648.

--- music/music_controller.gd
+++ music/music_controller.gd
@@ -1,11 +1,10 @@
 extends Node
 
-var bgm = preload("res://assets/sounds/background_music/Peritune_Black_Crystal.ogg")
+var bgm = load("res://assets/Music/background_music/Peritune_Black_Crystal.ogg")
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
-func play_bg_music() -> void:
+func play_music():
 	$Music.stream = bgm
-	$Music.volume_db = -20
 	$Music.play()
