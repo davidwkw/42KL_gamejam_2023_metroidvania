d8c3581d3fc2cd2a3ad9245bd9cd3d2ab1d87d75
Wang Khang Waei <devtesting404@gmail.com>
2023-11-29 20:44:13 +0800

    update: update slime tracking and enable slime death from stomp

--- mobs/slime.gd
+++ mobs/slime.gd
@@ -4,12 +4,8 @@ var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 var player : CharacterBody2D
 var chase : bool = false
 
-@export var move_speed : float = 50.0
+@export var move_speed : float = 100.0
 @onready var anim_player: AnimationPlayer = get_node("AnimationPlayer")
-
-func _on_player_detection_body_entered(body):
-	if body.name == "Player":
-		chase = true
 
 func _physics_process(delta):
 	if not is_on_floor():
@@ -19,23 +15,26 @@ func _physics_process(delta):
 		player = get_node("../../Player/Player")
 		var direction = (player.position - self.position).normalized()
 		direction_handler(direction.x)
-		if direction.x > 0:
-			self.velocity.x += move_speed
-		elif direction.x < 0:
-			self.velocity.x -= move_speed
-		else:
-			self.velocity.x = 0
 		if direction.x != 0:
-			anim_player.play("Move")
+			if anim_player.current_animation != "Death":
+				anim_player.play("Move")
+			self.velocity.x = move_speed * direction.x
+	else:
+		if anim_player.current_animation != "Death":
+			anim_player.play("Idle")
+		self.velocity.x = move_toward(self.velocity.x, 0, move_speed)
 		
 	move_and_slide()
 
+func _on_player_detection_body_entered(body):
+	if body.name == "Player":
+		chase = true
 
 func _on_player_detection_body_exited(body):
 	if body.name == "Player":
 		chase = false
 
-func direction_handler(direction : int):
+func direction_handler(direction : float):
 	var anim_sprite : AnimatedSprite2D = $AnimatedSprite2D
 	
 	if direction > 0:
@@ -44,3 +43,9 @@ func direction_handler(direction : int):
 		anim_sprite.flip_h = true
 	else: 
 		pass
+
+func _on_damage_hitbox_body_entered(body):
+	if body.name == "Player":
+		anim_player.play("Death")
+		await anim_player.animation_finished
+		self.queue_free()
