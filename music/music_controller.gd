29522f1439d9088e7a7d70706a273f4aba039531
Wang Khang Waei <devtesting404@gmail.com>
2023-11-30 16:12:09 +0800

    update: add return type to play_bg_music

--- music/music_controller.gd
+++ music/music_controller.gd
@@ -1,10 +1,11 @@
 extends Node
 
-var bgm = load("res://assets/Music/background_music/Peritune_Black_Crystal.ogg")
+var bgm = load("res://assets/sounds/background_music/Peritune_Black_Crystal.ogg")
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
-func play_music():
+func play_bg_music() -> void:
 	$Music.stream = bgm
+	$Music.volume_db = -20.0
 	$Music.play()
